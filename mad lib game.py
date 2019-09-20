451f38bcd8bebab2913b4ecba666835380842482
akash suluru <you@example.com>
2019-09-20 19:59:30 +0530

    this is a mad lib game

---
+++ mad lib game.py
@@ -0,0 +1,9 @@
+color = input("enter a color: ")
+Plural_noun = input("enter a plural noun: ")
+television_show = input("enter a television show: ")
+
+
+
+print(" oranges are " + color)
+print(Plural_noun + " are black ")
+print(" gerwe all love " + television_show)
